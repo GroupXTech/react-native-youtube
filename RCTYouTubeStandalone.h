f6153f0d0424eabb478df594dff2be78884b55be
Jim Reynolds <jim@groupxondemand.com>
2019-01-19 12:24:02 -0500

    added ondestroy method, changed to webkit

--- RCTYouTubeStandalone.h
+++ RCTYouTubeStandalone.h
@@ -8,8 +8,6 @@
 
 @interface RCTYouTubeStandalone: NSObject <RCTBridgeModule>
 
-#ifdef XCD_YOUTUBE_KIT_INSTALLED
-    - (void) moviePlayerPlaybackDidFinish:(NSNotification *)notification;
-#endif
+- (void) moviePlayerPlaybackDidFinish:(NSNotification *)notification;
 
 @end
--- example/index.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import RCTYouTubeExample from './RCTYouTubeExample';
-
-AppRegistry.registerComponent('RCTYouTubeExample', () => RCTYouTubeExample);
